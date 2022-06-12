77bd3be6cc736207d1f0e736c9581917c05bdcf0
rohantare03 <rohatare123@gmail.com>
2022-06-12 09:48:18 +0530

    [Rohan] Add : Creating a Table

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -1,2 +1,10 @@
 create database Payroll_Service;
 use Payroll_Service;
+
+create table employee_Payroll
+(
+	ID int identity (1,1) primary key,
+	Name varchar (20) NOT NULL,
+	salary int,
+	StartDate Date
+);
