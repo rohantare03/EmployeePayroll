958d9490828e5b983b5b412f432ac63e8e3c5876
rohantare03 <rohatare123@gmail.com>
2022-06-12 10:02:11 +0530

    [Rohan] Add : Retrieving All the Data

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -13,3 +13,5 @@ Insert into employee_Payroll values('Rohan', 3000, '2021-10-03')
 Insert into employee_Payroll values('Hades', 2500, '2022-02-22')
 Insert into employee_Payroll values('Athena', 4000, '2021-12-10')
 Insert into employee_Payroll values('Zeus', 2700, '2022-01-15')
+
+select * from employee_Payroll
